1717649ea093e212de05dd616f4255476e003026
David Martin <david.martin@tookay.net>
2025-01-02 00:07:55 +0000

    completed day-1.
    Add tests tomorrow and find a better way.

--- day1.js
+++ day1.js
@@ -3,3 +3,18 @@
 // Return the number (count) of vowels in the given string.
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 // The input string will only consist of lower case letters and/or spaces.
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const vowelCount = (str) => {
+    let count = 0;
+    let strArray = str.split("");
+
+    strArray.forEach((element) => {
+        if (vowels.indexOf(element) !== -1) count++;
+    })
+    return count
+}
+
+console.log(vowelCount('abcdefghijklm'))
+console.log(vowelCount('a ba ae cdefghijklm'))
