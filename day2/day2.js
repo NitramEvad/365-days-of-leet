3cad18379438ec61bdf17c3910f02b6a41d559d7
David Martin <david.martin@tookay.net>
2025-01-02 19:29:50 +0000

    super simple day 2 problem and test case

--- day2/day2.js
+++ day2/day2.js
@@ -1,2 +1,11 @@
 // keeping it simple for day2 also - technically still on holiday and will aim for a load of 7th kyu in January
 
+// DAY2: pass two strings as arguments to the function. If the first ends with the second, return TRUE.
+
+// very simple but first time I've used (and seen) the endsWith string method.
+
+function solution (str, endStr) {
+    return str.toLowerCase().endsWith(endStr.toLowerCase())
+}
+
+module.exports = solution;
