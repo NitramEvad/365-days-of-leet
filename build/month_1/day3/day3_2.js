c48791f833f6b0b9e6d288b3121d6f60bebe7a5b
David Martin <david.martin@tookay.net>
2025-01-07 10:26:29 +0000

    CHORE/remove unused build file

--- build/month_1/day3/day3_2.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.openOrSenior2 = void 0;
-const openOrSenior2 = data => {
-    const MINIMUM_AGE = 55;
-    const MINIMUM_HANDICAP = 7;
-    return data.map(([age, handicap]) => (age >= MINIMUM_AGE && handicap > MINIMUM_HANDICAP) ? 'Senior' : 'Open');
-};
-exports.openOrSenior2 = openOrSenior2;
