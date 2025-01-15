dcc4a7c72f68d44c42c50c186fd5a22382f3bd85
David Martin <david.martin@tookay.net>
2025-01-15 12:59:52 +0000

    CHORE/updated comments

--- src/month_1/day15/day15.ts
+++ src/month_1/day15/day15.ts
@@ -1,3 +1,11 @@
+/**
+ * https://www.codewars.com/kata/5751aa92f2dac7695d000fb0/javascript
+ * 
+ * Got bogged down trying to make this reusable for any contents in the 
+ * beastHeads and beastTails objects.
+ * So went with simple solution
+ */
+
 export function beasts (heads:number, tails:number):number[] | string {
     const beastHeads = {orthus: 2, hydra: 5, }
     const beastTails = {orthus: 1, hydra: 1, }
