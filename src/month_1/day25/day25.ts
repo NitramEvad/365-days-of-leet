e2255eacac9baac92c45cd7394d113c3db4e0bd1
David Martin <david.martin@tookay.net>
2025-01-25 23:15:48 +0000

    FEAT/day 25 - skeliton

---
+++ src/month_1/day25/day25.ts
@@ -0,0 +1,11 @@
+/*
+https://dev.to/dpc/daily-javascript-challenge-js-36-convert-time-in-24-hour-format-to-12-hour-format-211e
+The Challenge
+Difficulty: Easy
+
+Topic: Time Conversion
+
+Description
+Create a function that converts a string representing a time in the 24-hour format into a 12-hour format with an AM/PM suffix. The input string will be in the format 'HH:MM', where 'HH' is a two-digit hour, and 'MM' is a two-digit minute. The function should return a formatted string in the 12-hour format, e.g., '03:15 PM' or '12:00 AM'.
+*/
+
