05c4bde7d629c59bd80feab6f9205f7300e3c041
David Martin <david.martin@tookay.net>
2025-01-26 23:30:38 +0000

    FEAT/day 26 improve tests

--- src/month_1/day26/day26.test.ts
+++ src/month_1/day26/day26.test.ts
@@ -3,7 +3,8 @@ import longestSubString from "../day26/day26"
 describe('Day 27 tests', () => {
   test.each([
     ['a',1],
-    ['aa',1],
+    ['aa',0],
+    ['aaaaa',0],
     ['aab',2],
     ['bbaa',2],
     ['abcddefgghbznfgeeh',8]
