fb3057c4d7692166b9b9674267c5659b379575d3
David Martin <david.martin@tookay.net>
2025-01-29 17:47:02 +0000

    feat: can't even remember what/why updated

--- src/month_1/day28/day28.ts
+++ src/month_1/day28/day28.ts
@@ -8,3 +8,26 @@ Topic: Dynamic Programming
 Description
 Write a function to find the length of the longest increasing subsequence in an array of integers. A subsequence is derived by deleting some or no elements of the array without changing the order of the remaining elements.
 */
+
+export default function longestSubSeq (str:string):number {
+  // add each possible subsequence to array then pick longest
+  
+  const subSequences:number[][] = [];
+  const strArr = str.split('').map((el) => +el);
+  
+  strArr.forEach((el, idx) => {
+    const curArr:number[] = [el]
+
+    for (let i = idx; i < strArr.length; i++ ) {
+      if (strArr[i+1] && +strArr[i+1] > curArr[curArr.length-1]) curArr.push(+strArr[i+1])
+    }
+    subSequences.push(curArr)
+  })
+
+  const longest = subSequences.reduce((acc, cur) => {
+    if (cur.length > acc) acc = cur.length;
+    return acc;
+  }, 0);
+  
+  return longest
+}
