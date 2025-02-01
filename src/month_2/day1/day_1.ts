cb2fcdb9a5da61a8d34cb8db520a68db219e5386
David Martin <david.martin@tookay.net>
2025-02-01 18:10:33 +0000

    feat: add boilerplat function

---
+++ src/month_2/day1/day_1.ts
@@ -0,0 +1,8 @@
+/**
+ * Easy start to month 2 with https://dev.to/dpc/daily-javascript-challenge-js-86-array-symmetry-checker-34pj
+ */
+
+export default function symetricalArr (arr:string[]|number[]):boolean {
+
+    return true
+}
