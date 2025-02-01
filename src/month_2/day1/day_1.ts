3d4a5b7f3dbbb0590aaa74782161f6451ea5d332
David Martin <david.martin@tookay.net>
2025-02-01 19:58:53 +0000

    feat: revise to check against reversed array

--- src/month_2/day1/day_1.ts
+++ src/month_2/day1/day_1.ts
@@ -6,19 +6,14 @@ type StringOrNumberArray = (string | number )[];
 
 export default function symetricalArr (arr:StringOrNumberArray):boolean {
 
-  const length = arr.length-1;
-  const mid = arr.length/2;
-  let result = true;
-  let index = 0;
-  
-  while (result === true && index <= mid) {
-    if (arr[index] === arr[length-index]) {
-      index ++
-      continue
-    }
-  result = false
-  }
+  const reversedArr = [...arr].reverse();
+  let result = true
+
+  arr.forEach((el, index:number) => {
+    if (el === reversedArr[index]) result = false
+  })
+
   return result
 }
 
-console.log(symetricalArr([1,2,2,1]))
+console.log(symetricalArr([1,2,2,1,3]))
