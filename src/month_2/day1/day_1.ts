e32253989ab454f11b3a501d8b1f043a170b48aa
David Martin <david.martin@tookay.net>
2025-02-01 18:42:15 +0000

    feat: add basic symetry checker

--- src/month_2/day1/day_1.ts
+++ src/month_2/day1/day_1.ts
@@ -2,7 +2,23 @@
  * Easy start to month 2 with https://dev.to/dpc/daily-javascript-challenge-js-86-array-symmetry-checker-34pj
  */
 
-export default function symetricalArr (arr:string[]|number[]):boolean {
+type StringOrNumberArray = (string | number )[];
 
-    return true
+export default function symetricalArr (arr:StringOrNumberArray):boolean {
+
+  const length = arr.length-1;
+  const mid = arr.length/2;
+  let result = true;
+  let index = 0;
+  
+  while (result === true && index <= mid) {
+    if (arr[index] === arr[length-index]) {
+      index ++
+      continue
+    }
+  result = false
+  }
+  return result
 }
+
+console.log(symetricalArr([1,2,2,1]))
