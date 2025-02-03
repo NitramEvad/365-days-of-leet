5b72c67b9be4ca092d8f607383ac408be7b38469
David Martin <david.martin@tookay.net>
2025-02-03 18:50:51 +0000

    test: add second test for revised function

--- src/month_2/day3/day_3.test.ts
+++ src/month_2/day3/day_3.test.ts
@@ -1,4 +1,4 @@
-import { progressionCheck } from './day_3'
+import { progressionCheck, progressionCheckv2 } from './day_3'
 describe('Day 3 tests', () => {
   test.each([
     [[0],true],
@@ -12,5 +12,18 @@ describe('Day 3 tests', () => {
     [[-4,-2,-4, 2, 0, 2,4],false],
   ])('returns %s for %s', (input, output) => {
     expect(progressionCheck(input)).toEqual(output)
+  }),
+  test.each([
+    [[0],true],
+    [[5,2,4,3,1],true],
+    [[1,2,3,4,5],true],
+    [[0,2,4,6,8],true],
+    [[8,6,4,2,0],true],
+    [[-8,-6,-4,-2,0],true],
+    [[8,6,4,2,0, -2, -4],true],
+    [[-4,-2,0,2,4,6],true],
+    [[-4,-2,-4, 2, 0, 2,4],false],
+  ])('returns %s for %s', (input, output) => {
+    expect(progressionCheckv2(input)).toEqual(output)
   })
 })
