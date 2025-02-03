a3872c9f79b10c5a11ede99be37dcf922155e0f1
David Martin <david.martin@tookay.net>
2025-02-03 18:37:29 +0000

    test: add test for day 3

---
+++ src/month_2/day3/day_3.test.ts
@@ -0,0 +1,16 @@
+import { progressionCheck } from './day_3'
+describe('Day 3 tests', () => {
+  test.each([
+    [[0],true],
+    [[5,2,4,3,1],true],
+    [[1,2,3,4,5],true],
+    [[0,2,4,6,8],true],
+    [[8,6,4,2,0],true],
+    [[-8,-6,-4,-2,0],true],
+    [[8,6,4,2,0, -2, -4],true],
+    [[-4,-2,0,2,4,6],true],
+    [[-4,-2,-4, 2, 0, 2,4],false],
+  ])('returns %s for %s', (input, output) => {
+    expect(progressionCheck(input)).toEqual(output)
+  })
+})
