f1d7006faead8e13f33fbe7b1b7f952dcf20f36b
David Martin <david.martin@tookay.net>
2025-02-04 16:46:24 +0000

    refactor: back to day 31, will restart it

--- src/month_1/day31/day31.ts
+++ src/month_1/day31/day31.ts
@@ -9,27 +9,50 @@ Description
 Given a string consisting of various delimiter characters such as parentheses '()', square brackets '[]', and curly braces '{}', determine if the delimiters are balanced. A balanced delimiter means that for every opening delimiter, there exists a corresponding closing delimiter that comes afterwards, and they are correctly nested.
 */
 
-export default function delimiters(str:string):boolean {
-  const delimiterObj = {
-    '{': 0,
-    '}': 0,
-    '[': 0,
-    ']': 0,
-    '(': 0,
-    ')': 0,
+import { off } from "process"
+
+export default function delimiters(str:string, result?:boolean):boolean {
+  const delimiterObj: { [key: string]: string } = {
+    '{': '}',
+    '}': '{',
+    '[': ']',
+    ']': '[',
+    '(': ')',
+    ')': '(',
   }
 
-// Find last open item
-// move forward until you find corresponding item return true if so
-// if another open delim is detected recurse
-
-  str
+  // eliminate uneeded characters from string
+  const strArr = str
     .split('')
-    .forEach((el) => {
-      if (delimiterObj[el])
-    })
-  
-    return true
+    .filter((key) => key in delimiterObj)
+      
+  strArr.forEach((el, idx) => {
+    
+    console.log(idx, result, strArr)
+
+    if (delimiterObj[el] === strArr[idx+1]) {
+      let newArr = [
+        ...strArr.slice(0, idx),
+        ...strArr.slice(idx+2),
+      ]
+      console.log('rerun')
+      delimiters(newArr.join(''), false)
+    } else if (!strArr) {
+      console.log('success')
+      result = true
+      return true
+    } else if (idx+1 === strArr.length) {
+    console.log('failure')
+    result = false
+    return false
+    }
+  })
+
+  console.log('END: ', strArr, result)0
+  if (result) return true
+  return false
 } 
 
-console.log(delimiters('abc{def}ab()c[d[  d'))
+console.log(delimiters('{{{()'))
+// console.log(delimiters('a{bc{def}a}b()c[d[  d'))
+// console.log(delimiters('az(ads(ab{df}f)fdf'))
