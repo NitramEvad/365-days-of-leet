f39776811a12b0a42caaebdf0a3f028df93c4e43
David Martin <david.martin@tookay.net>
2025-02-05 10:02:32 +0000

    refactor: add more test cases

--- src/month_2/day5/day_5.test.ts
+++ src/month_2/day5/day_5.test.ts
@@ -1,11 +1,34 @@
 import ticTacToe from "./day_5"
 describe('Day 5 tests', () => {
   test.each([
-    [[
-      ['X','O','X'],
-      ['O','X','O'],
-      ['O','X','X']
-    ], true]
+    [
+      [
+        ['X','O','X'],
+        ['O','X','O'],
+        ['O','X','X']
+      ], true
+    ],
+    [
+      [
+        ['X','O','O'],
+        ['X','O','O'],
+        ['X','X','X']
+      ], true
+    ],
+    [
+      [
+        ['X','X','O'],
+        ['X','X','O'],
+        ['X','X','O']
+      ], false
+    ],
+    [
+      [
+        ['X','O','O'],
+        ['','X','O'],
+        ['','O','X']
+      ], true
+    ],
   ])('%s should give %s', (input,output) => {
     expect(ticTacToe(input)).toEqual(output)
   })
