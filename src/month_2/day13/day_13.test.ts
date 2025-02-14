504aab85c3ea54adf67a48e4c33ea18290fda39f
David Martin <david.martin@tookay.net>
2025-02-14 00:00:45 +0000

    test: add test cases

--- src/month_2/day13/day_13.test.ts
+++ src/month_2/day13/day_13.test.ts
@@ -1,5 +1,11 @@
 import cumulativeSum from "./day_13";
 
 describe('Day 3 tests', () => {
-  
+  test.each([
+    [[1,2,3,4],10],
+    [[0,0,0],0],
+    [[-1, -2, -5, 3],-5],
+  ])('%s should equal %i', (input, output) => {
+    expect(cumulativeSum(input)).toEqual(output)
+  })
 })
