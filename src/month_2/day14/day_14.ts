e05ae66131f5535bdfa8992aa73c148b09c62be4
David Martin <david.martin@tookay.net>
2025-02-14 18:45:56 +0000

    refactor: more efficient version

--- src/month_2/day14/day_14.ts
+++ src/month_2/day14/day_14.ts
@@ -4,18 +4,16 @@
  */
 
 export function pallindrome (str:string):boolean {
-  const regex = /[a-z0-9]/g;
-  const cleanStr = str.toLowerCase().split('').filter((el) => el.match(regex));
-  const reversedStr  = [...cleanStr].reverse()
+  const regex = /[^a-z0-9]/g;
+  str = str.toLowerCase().replace(regex, '');
 
-  let isPallindrome = true;
-  let index = 0;
+  let i = 0;
+  let j = str.length-1;
 
-  while (isPallindrome === true && index < cleanStr.length/2) {
-    if (cleanStr[index] !== reversedStr[index]) isPallindrome = false
-    index++
+  while (i < j) {
+    if (str[i] !== str[j]) return false;
+    i++
+    j--
   }
-  return isPallindrome
+  return true
 }
-
-
