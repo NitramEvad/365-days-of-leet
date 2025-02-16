3dc3675d56de4fd7ee8a6ff87bf3cbcf14070c8f
David Martin <david.martin@tookay.net>
2025-02-16 23:41:28 +0000

    feat: day 16, nice and easy

--- src/month_2/day16/day_16.ts
+++ src/month_2/day16/day_16.ts
@@ -8,3 +8,18 @@
  * Description
  * Write a function that takes a string as input and returns a new string with all consecutive duplicate characters removed. The order of the characters should be preserved except for consecutive duplicates, which should be reduced to a single character.
  */
+
+export function removeDuplicates (str:string):string {
+  if (str.length === 0) return '';
+  let result = str[0];
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i+1]) {
+      result += str[i]
+    }
+  }
+  
+  return result
+}
+
+console.log(removeDuplicates('davidwill be innn thhhhhheeeee'))
