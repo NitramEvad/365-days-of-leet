1618b86ee451b9f73a05422d22e57fe97ab303cf
David Martin <david.martin@tookay.net>
2025-02-18 23:52:54 +0000

    feat: day 18 challenge

--- src/month_2/day18/day_18.ts
+++ src/month_2/day18/day_18.ts
@@ -9,3 +9,22 @@
  * Description
  * Create a function that takes an array of arrays and returns an array containing the common elements across all arrays. Elements should be present in all arrays to be included in the result. The result should not have duplicate elements.
  */
+
+export function commonElements (arr: (string|number)[][]):(string|number)[] {
+  
+  // removes duplicates
+  const formatted = Array.from(new Set(arr[0]))
+
+  let result:(string|number)[] = []
+  
+  formatted.forEach((el) => {
+    let count = 1;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].indexOf(el) !== -1) count++
+    }
+    count === arr.length && result.push(el)
+  })
+
+  return result
+}
+
