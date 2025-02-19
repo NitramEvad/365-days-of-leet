3ed859f28636dd425734ee32535fb6f680c7b486
David Martin <david.martin@tookay.net>
2025-02-19 16:19:31 +0000

    feat: day 20 challenge - will refactor

--- src/month_2/day20/day_20.ts
+++ src/month_2/day20/day_20.ts
@@ -9,3 +9,43 @@
  * Description
  * Given an array that represents an arithmetic sequence with exactly one number missing, find the missing number in the sequence. An arithmetic sequence is a sequence of numbers with a constant difference between consecutive terms.
  */
+
+export function sequence (arr:number[]):number|string {
+
+  const variabilityObj = {};
+  let variability;
+  let result;
+
+  
+
+  // tallies variability in to an object
+  arr.sort((a,b) => a-b).reduce((acc:number, cur:number) => {
+    let diff = cur - acc;
+    // variabilityObj[diff] ? variabilityObj[diff]++ : variabilityObj[diff] = 1;
+    variabilityObj[diff] = (variabilityObj[diff] ? variabilityObj[diff]+=1 : 1)
+    return acc = cur
+  })
+
+  // returns 0 if there is no or consistent variability
+  if (Object.keys(variabilityObj).length < 2) {
+    // throw new Error("variabilityObj is empty");
+    return 'No variability'
+  }
+  
+  // finds most frequent variability
+  for (const [key, val] of Object.entries(variabilityObj) as [string, number][]) {
+    if (val > 1) {
+      variability = key
+    }
+  }
+  
+  // find missing number
+  for (let i = 0; i < arr.length-1; i++) {
+    if (arr[i] + Number(variability) !== arr[i+1]) {
+      result = arr[i]+Number(variability)
+      break
+    }
+  }
+
+  return result
+}
