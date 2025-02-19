31308e8ef3f8b3b6453fea0d9ca248e585e11ace
David Martin <david.martin@tookay.net>
2025-02-19 14:28:33 +0000

    feat: day 19 challenge

--- src/month_2/day19/day_19.ts
+++ src/month_2/day19/day_19.ts
@@ -9,3 +9,15 @@
  * Description
  * Given an integer n, write a function that returns the nth row of Pascal's Triangle. Pascal's Triangle is a triangular array of numbers, such that every number is the sum of the two numbers directly above it in the previous row. The 0th row is [1].
  */
+
+export function pascale (num: number):number[] {
+  let result:number[] = [1]
+  if (num === 1) return result
+
+  for (let i = 0; i < num; i++) {
+    result.push(Math.floor(result[i]*((num - i) / (i+1))))
+  }
+
+  return result
+}
+
