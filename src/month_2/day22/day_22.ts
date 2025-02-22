236ff5c216063adf5143df1c59045aa164a53fc8
David Martin <david.martin@tookay.net>
2025-02-22 20:02:12 +0000

    feat: day 22 challenge

--- src/month_2/day22/day_22.ts
+++ src/month_2/day22/day_22.ts
@@ -9,3 +9,14 @@
  * Description
  * Given an unsorted array of integers, find the smallest positive integer that does not appear in the array. Ignore negative numbers and zero, and optimize for time and space complexity.
  */
+
+export function smallestInt (arr:number[]):number {
+
+  let smallest = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === 1) return 0 // early return
+    if (arr[i] < smallest && arr[i] > 0) smallest = arr[i]
+  }
+  
+  return smallest-1
+}
