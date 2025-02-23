aaac4e60f526c6ffd70bf4bca2bce0f7114d68da
David Martin <david.martin@tookay.net>
2025-02-23 17:37:15 +0000

    feat: day 23 challenge

--- src/month_2/day23/day_23.ts
+++ src/month_2/day23/day_23.ts
@@ -9,3 +9,12 @@
  * Description
  * Create a function that takes an array of integers as an input and returns the first element in the array that is not consecutive. By consecutive, we mean that the number following one element should be exactly 1 greater than the current element. If the entire array is made up of consecutive numbers, return null.
  */
+
+export function firstConsecutive (arr:number[]):number|null {
+
+  for (let i = 0; i < arr.length-1; i++) {
+    if (arr[i+1] - arr[i] !== 1) return arr[i+1]
+  }
+
+  return null
+}
