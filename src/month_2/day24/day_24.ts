2ef85e6a49d744730711640ff0dd058e7c72ed3b
David Martin <david.martin@tookay.net>
2025-02-24 23:34:01 +0000

    feat: day 24 challenge

--- src/month_2/day24/day_24.ts
+++ src/month_2/day24/day_24.ts
@@ -9,3 +9,27 @@
  * Description
  * Given a circular array of integers, find the next greater element for each element. The next greater element is the first element greater than the current one that appears in circular traversal of the array. If it doesn't exist, return -1 for that element.
  */
+
+export function circularTraversal (arr:number[]):number[] {
+
+  let stack:number[] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    stack.push(arr[i])
+    let nextIndex = i;
+    
+    do {
+      nextIndex = nextIndex + 1 === arr.length ? 0 : nextIndex + 1;
+    } while (nextIndex !== i && arr[nextIndex] <= stack[i])
+
+    if (arr[nextIndex] > stack[i]) {
+      stack.pop()
+      stack.push(arr[nextIndex])
+    } else if (nextIndex === i) {
+      stack.pop()
+      stack.push(-1)
+    }
+  }
+
+  return stack
+}
