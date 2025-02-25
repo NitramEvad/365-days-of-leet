f67c17133d570924d43155c4c698e2b6aa518116
David Martin <david.martin@tookay.net>
2025-02-25 23:02:53 +0000

    feat: day 25 challenge

--- src/month_2/day25/day_25.ts
+++ src/month_2/day25/day_25.ts
@@ -9,3 +9,11 @@
  * Description
  * Write a function to calculate the N-th Fibonacci number. The Fibonacci sequence is defined as follows: ( F(0) = 0 ), ( F(1) = 1 ), and ( F(n) = F(n-1) + F(n-2) ) for ( n > 1 ). The function should efficiently compute the Fibonacci numbers for large N (e.g., N = 50).
  */
+
+export function fib (n:number):number {
+    if (n === 0) return 0
+    if (n < 3) return 1
+    return (fib(n-2)+fib(n-1))
+}
+
+console.log(fib(4))
