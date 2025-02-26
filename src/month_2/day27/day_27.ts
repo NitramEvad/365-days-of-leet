b9a38b4bfa56e1611308aecc943dd2a45da903df
David Martin <david.martin@tookay.net>
2025-02-26 23:51:26 +0000

    feat: day 27 challenge (WIP)

--- src/month_2/day27/day_27.ts
+++ src/month_2/day27/day_27.ts
@@ -9,3 +9,26 @@
  * Description
  * Given an array of integers where every integer appears twice except for two integers, find the two integers that appear only once.
  */
+
+export function singles (arr:number[]):number[] {
+
+  let result:number[] = [];
+  let sortedArr = [...arr].sort((a,b) => a - b)
+  let index = 0;
+  console.log(sortedArr)
+
+  while (index < sortedArr.length-1 &&  result.length < 2) { 
+    if (sortedArr[index+1] !== sortedArr[index]) {
+      result.push(sortedArr[index])
+      index += 1
+      console.log('A', index, result)
+    } else {
+      index +=2
+      console.log('B', index, result)
+    }
+  }
+
+  return result
+}
+
+singles([1,2,3,4,3,5,2,4,])
