9d4fb13f3240f9c76fd07799a6efca74beb4285b
David Martin <david.martin@tookay.net>
2025-02-27 14:28:31 +0000

    feat: day 26 challenge

--- src/month_2/day26/day_26.ts
+++ src/month_2/day26/day_26.ts
@@ -1,5 +1,5 @@
 /**
- * https://dev.to/dpc/daily-javascript-challenge-js-63-find-maximum-subarray-sum-with-a-twist-12ca
+ * https://dev.to/dpc/daily-javascript-challenge-js-51-merge-overlapping-intervals-4g6i
  * 
  * The Challenge
  * Difficulty: Medium
@@ -7,5 +7,23 @@
  * Topic: Array Manipulation
  * 
  * Description
- * Given an array of integers, find the maximum sum of any contiguous subarray. Additionally, if a subarray sum becomes negative, it should be changed to zero, effectively treating that segment as if it was not part of the array.
+ * Given an unsorted array of integers, find the length of the longest increasing subsequence.
  */
+
+export function subSeq (arr:number[]):number {
+
+  let longest = 0;
+  let length = 1;
+
+  arr.reduce((acc:number, cur:number):number => {
+    if (cur > acc) {
+      length += 1;
+    } else {
+      if (length > longest) longest = length;
+      length = 1;
+    }
+    return cur
+  })
+
+  return longest
+}
