9f8f40f51e1dc00886c90a22f847ac15952389ae
David Martin <david.martin@tookay.net>
2025-02-27 10:56:35 +0000

    feat: further test case

--- src/month_2/day27/day_27.test.ts
+++ src/month_2/day27/day_27.test.ts
@@ -2,6 +2,7 @@ import { singles } from './day_27'
 
 describe('Day 27 tests', () => {
   test.each([
+    [[0,1,2,0],[1,2]],
     [[1,2,3,4,3,5,2,4,],[1,5]],
     [[1,2,4,3,2,4,],[1,3]],
   ])('%s should return %s', (input, output) => {
