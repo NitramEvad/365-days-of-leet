c6b8b9fea8d99207602050ffeab3d73f559b4b53
David Martin <david.martin@tookay.net>
2025-02-27 14:28:47 +0000

    test: tidy up unused test cases

--- src/month_2/day26/day_26.test.ts
+++ src/month_2/day26/day_26.test.ts
@@ -6,8 +6,6 @@ describe('Day 26 tests', () => {
     [[1,1,1,1],1],
     [[1,2,5,3],3],
     [[1,2,5,3,4,5,1,5,12,13,15,14,16],5],
-    // [[],],
-    // [[],],
   ])('%s should equal %i', (input, output) => {
     expect(subSeq(input)).toEqual(output)
   })
