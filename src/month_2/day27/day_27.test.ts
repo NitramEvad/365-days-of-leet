2ceb71ccd6e97bf73432ed727d5c8a22ca7adebd
David Martin <david.martin@tookay.net>
2025-02-27 18:09:59 +0000

    test: add vitest

--- src/month_2/day27/day_27.test.ts
+++ src/month_2/day27/day_27.test.ts
@@ -1,11 +1,16 @@
 import { singles } from './day_27'
+import { expect, test } from 'vitest'
 
-describe('Day 27 tests', () => {
-  test.each([
-    [[0,1,2,0],[1,2]],
-    [[1,2,3,4,3,5,2,4,],[1,5]],
-    [[1,2,4,3,2,4,],[1,3]],
-  ])('%s should return %s', (input, output) => {
-    expect(singles(input)).toEqual(output)
-  })
+// describe('Day 27 tests', () => {
+//   test.each([
+//     [[0,1,2,0],[1,2]],
+//     [[1,2,3,4,3,5,2,4,],[1,5]],
+//     [[1,2,4,3,2,4,],[1,3]],
+//   ])('%s should return %s', (input, output) => {
+//     expect(singles(input)).toEqual(output)
+//   })
+// })
+
+test('Day 27 with Vitest', () => {
+  expect(singles([1,2,4,3,2,4,])).toEqual([1,3])
 })
