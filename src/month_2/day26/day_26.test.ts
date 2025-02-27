25784689542562e7a380de6814ab249e41481717
David Martin <david.martin@tookay.net>
2025-02-27 14:27:57 +0000

    test: day 26 tests

---
+++ src/month_2/day26/day_26.test.ts
@@ -0,0 +1,14 @@
+import { subSeq } from './day_26'
+
+describe('Day 26 tests', () => {
+  test.each([
+    [[0],0],
+    [[1,1,1,1],1],
+    [[1,2,5,3],3],
+    [[1,2,5,3,4,5,1,5,12,13,15,14,16],5],
+    // [[],],
+    // [[],],
+  ])('%s should equal %i', (input, output) => {
+    expect(subSeq(input)).toEqual(output)
+  })
+})
