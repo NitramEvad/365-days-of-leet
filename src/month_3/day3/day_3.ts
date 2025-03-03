5aa91428029abce4325e055d375bd2fcbd5dcfc1
David Martin <david.martin@tookay.net>
2025-03-03 21:35:58 +0000

    feat: day 3 challenge - familiar

--- src/month_3/day3/day_3.ts
+++ src/month_3/day3/day_3.ts
@@ -9,3 +9,21 @@
  * Description
  * Given an array of integers where every integer appears twice except for two integers, find the two integers that appear only once.
  */
+
+export function singleInstances (arr:number[]):number[] {
+
+  let instances = {};
+  let resultArr:number[] = [];
+
+  arr.forEach((el) => {
+    instances[el] ? instances[el] += 1 : instances[el] = 1
+  })
+
+  for (const key in instances) {
+    if (instances[key] === 1) resultArr.push(Number(key))
+  }
+
+  if (resultArr.length !== 2) return []
+
+  return resultArr
+}
