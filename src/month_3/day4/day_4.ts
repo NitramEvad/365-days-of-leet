c55e1d8c4c5e7fc9cb022747b76d8e17b50aaa41
David Martin <david.martin@tookay.net>
2025-03-06 15:53:54 +0000

    refactor: improve day 4 challenge

--- src/month_3/day4/day_4.ts
+++ src/month_3/day4/day_4.ts
@@ -14,22 +14,22 @@ export function primeFactors (int:number):number[] {
 
   let resultArr:number[] = []
 
+  // checks if given number is a prime 
   const checkPrime = (num:number):boolean => {
-    // produces a list of all primes up to and including int
-    if (num < 3 || num % 2 === 0) return false
+    if (num < 2 || num > 3 && num % 2 === 0) return false
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false
+    }
     return true
   }
 
-
+  // checks for factors
+  // then calls checkPrime to confirm if it is a prime
   for (let i = 1; i <= int; i++) {
     if (int % i === 0) {
       if (checkPrime(i)) resultArr.push(i)
-    //   if (checkPrime(int / i)) resultArr.push(int / i)
     }
   }
 
   return resultArr
-
 }
-
-console.log(primeFactors(12))
