2e5fade88d206c52936327704c7e814a992bf27c
David Martin <david.martin@tookay.net>
2025-03-06 21:24:49 +0000

    feat: day 6 challenge

---
+++ src/month_3/day6/day_6.ts
@@ -0,0 +1,15 @@
+/**
+The Challenge
+Difficulty: Medium
+
+Topic: Conversion
+
+Description
+Write a function that converts a given string representing a hexadecimal number into its binary representation.
+ */
+
+export function hexToBinary (str:string):string {
+
+  return parseInt(str, 16).toString(2)
+
+}
