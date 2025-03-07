1aeec2b2b0f23395cc033056d545e023702cb4f5
David Martin <david.martin@tookay.net>
2025-03-07 16:45:48 +0000

    refactor: improve palindrome check

--- src/month_3/day7/day_7.ts
+++ src/month_3/day7/day_7.ts
@@ -9,5 +9,21 @@ Given a string, determine if any permutation of the string is a palindrome. A pa
  */
 
 export function isPalindrome (str:string):boolean {
-  return true
+
+  let palindrome = true;
+  let shift = 0;
+  const filteredStr = str.toLowerCase().replace(/ /g, '')
+  const length = filteredStr.length-1; 
+
+  while (palindrome && shift <= length/2) {
+    palindrome = filteredStr[shift] === filteredStr[length-shift]
+    shift++
+  }
+
+  return palindrome
 }
+
+console.log(isPalindrome('theeht'))
+console.log(isPalindrome('theht'))
+console.log(isPalindrome('xtneht'))
+console.log(isPalindrome('x Ten etX'))
