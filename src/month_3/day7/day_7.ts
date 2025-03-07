702afbc7aa6c9531b23e01fe0634635e08fa1a89
David Martin <david.martin@tookay.net>
2025-03-07 07:39:13 +0000

    feat: boilerplate for day 7 challenge

---
+++ src/month_3/day7/day_7.ts
@@ -0,0 +1,13 @@
+/**
+ * The Challenge
+Difficulty: Medium
+
+Topic: String Manipulation
+
+Description
+Given a string, determine if any permutation of the string is a palindrome. A palindrome is a word that reads the same backward as forward.
+ */
+
+export function isPalindrome (str:string):boolean {
+  return true
+}
