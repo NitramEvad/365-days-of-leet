9bb3b52ad1d150c8bbb5893da747b6c8c5cdf330
David Martin <david.martin@tookay.net>
2025-03-07 16:45:33 +0000

    test: add more test cases

--- src/month_3/day7/day_7.test.ts
+++ src/month_3/day7/day_7.test.ts
@@ -3,17 +3,36 @@ import { isPalindrome } from "./day_7";
 describe('Day 7 tests', () => {
   test.each([
     ['race be car', true],
+    ['x Ten etX', true],
+    ['a', true],
+    ['aa',true],
+    ['abcba', true],
+    ['abccba', true],
+    ['abcd', false],
+    ['A man a plan a canal Panama', true],
+    ['No lemon no melon', true],
+    ['Was it a car or a cat I saw', true],
+    ['Not a palindrome', false]
   ])('"%s" should return %s', (input, output) => {
     expect(isPalindrome(input)).toEqual(output)
   })
   test.each([
     ['race be car'],
+    ['a'],
+    ['aa'],
+    ['abcba'],
+    ['abccba'],
+    ['A man a plan a canal Panama'],
+    ['No lemon no melon'],
+    ['Was it a car or a cat I saw']
   ])('"%s" should return TRUE', (input) => {
-    expect(isPalindrome(input)).toBeTruthy
+    expect(isPalindrome(input)).toBeTruthy()
   })
   test.each([
-    ['race be car'],
+    ['race bee car'],
+    ['abcd'],
+    ['Not a palindrome']
   ])('"%s" should return FALSE', (input) => {
-    expect(isPalindrome(input)).toBeFalsy
+    expect(isPalindrome(input)).toBeFalsy()
   })
 })
