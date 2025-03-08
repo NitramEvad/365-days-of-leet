2c308bc4688404fd6e33177ffd72e408056db7fb
David Martin <david.martin@tookay.net>
2025-03-08 21:18:44 +0000

    feat: day 8 setup

---
+++ src/month_3/day8/day_8.ts
@@ -0,0 +1,32 @@
+/**
+ * The Challenge
+ * Difficulty: Medium
+ * 
+ * Topic: Arrays and Sorting
+ * 
+ * Description
+ * You are given an array of objects, each object contains two properties: a 'name' (string) and an 'age' (integer). Write a function to sort this array in ascending order based on the 'age' property. If two objects have the same age, keep their original order in the sorted array.
+ */
+
+export const sorter = (arr) => {
+  return {}
+}
+
+console.log(sorter([
+  {
+    name: 'aaffgd',
+    age: 20,
+  },
+  {
+    name: 'bbdfgdfg',
+    age: 200,
+  },
+  {
+    name: 'bb-adfgdfg',
+    age: 200,
+  },
+  {
+    name: 'dfgdfgddfgdggg',
+    age: 10,
+  },
+]))
