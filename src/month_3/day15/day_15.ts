86fd629339a6964b931303816103236574a7661a
David Martin <david.martin@tookay.net>
2025-03-16 21:57:55 +0000

    feat: feels messy, must be an easier solution

--- src/month_3/day15/day_15.ts
+++ src/month_3/day15/day_15.ts
@@ -16,18 +16,12 @@ export function removeOverlaps (arr: number[][]):number[][] {
   let current:number[] = arr[0];
 
   for (let i = 1; i < arr.length; i++) {
-    console.log('\nX', current, arr[i])
     if (current[1] < arr[i][0]) { 
-      slimmedArr.push(arr[i])
-      console.log('CL1:', current, slimmedArr)
+      slimmedArr.push(current)
+      i === arr.length-1 ? slimmedArr.push(arr[i]) : current = arr[i]
     } else {
       current = [current[0], arr[i][1]]
-      console.log('CL2:', current, slimmedArr)
     }
   }
-
   return slimmedArr
 }
-
-console.log(removeOverlaps([[1,3],[4,7],[8,9]]))
-console.log(removeOverlaps([[1,4],[3,7],[8,9],[11,15],[12,17],[11,19],[30,40]]))
