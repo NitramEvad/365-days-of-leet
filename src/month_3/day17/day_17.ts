c8baffebe6f56262dcb8de9547ae3fdb7252ae14
David Martin <david.martin@tookay.net>
2025-03-17 17:04:06 +0000

    feat: day 17 challenge

---
+++ src/month_3/day17/day_17.ts
@@ -0,0 +1,29 @@
+/**
+ * https://dev.to/dpc/daily-javascript-challenge-js-85-sum-of-digits-until-single-digit-2n99
+ * Daily JavaScript Challenge: Sum of Digits Until Single Digit
+ * The Challenge
+ * Difficulty: Medium
+ *
+ * Topic: Number Manipulation
+ *
+ * Description
+ * Given a non-negative integer, repeatedly sum all its digits until the result is a single digit.
+ */
+
+
+export function sumOfDoubleDigits (num:number):number {
+
+  let current = num;
+
+  while (current >= 10) {
+    let numArr = current
+      .toString()
+      .split('')
+      .map(Number)
+
+    current = numArr.reduce((acc, cur) => acc + cur)
+  }
+  return current
+}
+
+sumOfDoubleDigits(10987654321)
