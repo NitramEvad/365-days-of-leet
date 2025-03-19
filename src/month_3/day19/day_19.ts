9e428cf5b97f381dc6401dd00d7767f0ade7d315
David Martin <david.martin@tookay.net>
2025-03-19 23:44:46 +0000

    feat: day 19 challenge

---
+++ src/month_3/day19/day_19.ts
@@ -0,0 +1,32 @@
+/**
+ * https://dev.to/dpc/daily-javascript-challenge-js-87-binary-gap-length-5o7
+ * 
+ * Daily JavaScript Challenge: Binary Gap Length
+ * 
+ * The Challenge
+ * Difficulty: Medium
+ * 
+ * Topic: Bit Manipulation
+ * 
+ * Description
+ * Write a function that takes an integer and returns the length of the longest sequence of consecutive zeros ("gaps") that are surrounded by ones in the binary representation of the given number. The function should return 0 if there are no gaps.
+ */
+
+export function bitGap (input: string | number):number {
+
+  const numConverted = typeof input === "number" ? input.toString(2) : input;
+
+  let largest = 0;
+  let current = 0;
+
+  for (let i = 0; i < numConverted.length; i++) {
+    if (numConverted[i] === '0') {
+      current +=1
+      if (current > largest) largest = current
+    } else {
+      current = 0;
+    }
+  }
+
+  return largest
+}
