bf2760e9efc5f92dfede4d105552772917fdb80c
David Martin <david.martin@tookay.net>
2025-03-25 16:45:09 +0000

    feat: day 24 - (WIP)

---
+++ src/month_3/day24/day_24.ts
@@ -0,0 +1,30 @@
+/**
+ * https://dev.to/dpc/daily-javascript-challenge-js-92-decode-a-string-with-nested-patterns-44da
+ * 
+ * Daily JavaScript Challenge: Decode a String with Nested Patterns
+ * 
+ * The Challenge
+ * Difficulty: Medium
+ * 
+ * Topic: String Manipulation
+ * 
+ * Description
+ * Given an encoded string where the pattern is defined as ${n}[sub_string], decode it by repeating sub_string n times. The encoded string may have nested patterns.
+ * 
+ */
+
+export function nestedPatterns (str:string):string {
+
+  const regex = /\$\{(\d+)\}\[([^\[\]]+)\]/g;
+
+  while (regex.test(str)) {
+
+    str = str.replace(regex, (_, count, substring) => {
+      console.log(_, count, substring);
+      return substring.repeat(Number(count));
+    });
+  }
+
+  return str;
+}
+console.log(nestedPatterns('abc efd ${5}[LLL MMMM ${5}[123 456 ] ] sfsdfsd ${5}[XXX YYY]dfgdfgfdg'))
