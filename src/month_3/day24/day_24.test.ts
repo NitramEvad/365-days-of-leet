7b2b2ec364da008c7d4879e92270ac622cb013a1
David Martin <david.martin@tookay.net>
2025-03-25 16:44:55 +0000

    test: day 24

--- src/month_3/day24/day_24.test.ts
+++ src/month_3/day24/day_24.test.ts
@@ -2,7 +2,10 @@ import { nestedPatterns } from "./day_24"
 
 describe('Day 24 tests', () => {
   test.each([
-    ['',''],
+    [
+      'abc efd ${5}[LLL MMMM ${5}[123 456 ] ] sfsdfsd ${5}[XXX YYY]dfgdfgfdg',
+      'abc efd LLL MMMM 123 456 123 456 123 456 123 456 123 456  LLL MMMM 123 456 123 456 123 456 123 456 123 456  LLL MMMM 123 456 123 456 123 456 123 456 123 456  LLL MMMM 123 456 123 456 123 456 123 456 123 456  LLL MMMM 123 456 123 456 123 456 123 456 123 456   sfsdfsd XXX YYYXXX YYYXXX YYYXXX YYYXXX YYYdfgdfgfdg'
+    ],
   ])('"%s" should return "%s"', (input, output) => {
     expect(nestedPatterns(input)).toEqual(output)
   })
