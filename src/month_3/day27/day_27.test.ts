979834a2ac419533ea7af661986b44bd67cfdeec
David Martin <david.martin@tookay.net>
2025-03-27 23:47:59 +0000

    test: add more test cases

--- src/month_3/day27/day_27.test.ts
+++ src/month_3/day27/day_27.test.ts
@@ -2,6 +2,11 @@ import { bracketBalancer } from "./day_27"
 
 describe('Day 24 tests', () => {
   test.each([
+    ['[]', true],
+    ['[]{}', true],
+    ['[]{}()', true],
+    ['{[{}]{[]}()()}', true],
+    ['([)]{}', false],
     ['sfsfd(sf[dfd]sd)sdf{sdfsf}fs[df]fdsd', true],
   ])('"%s" should return %s', (input, output) => {
     expect(bracketBalancer(input)).toEqual(output)
