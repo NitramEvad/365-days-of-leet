55e19f3001d7a608277f6810897d005b246dfb95
David Martin <david.martin@tookay.net>
2025-03-27 22:51:40 +0000

    test: day 27 tests

---
+++ src/month_3/day27/day_27.test.ts
@@ -0,0 +1,9 @@
+import { bracketBalancer } from "./day_27"
+
+describe('Day 24 tests', () => {
+  test.each([
+    ['sfsfd(sf[dfd]sd)sdf{sdfsf}fs[df]fdsd', true],
+  ])('"%s" should return %s', (input, output) => {
+    expect(bracketBalancer(input)).toEqual(output)
+  })
+})
