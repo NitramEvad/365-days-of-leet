36d9c3b68b4ae023c7d1d31274a0ee467626d238
David Martin <david.martin@tookay.net>
2025-03-28 11:17:37 +0000

    test: day 25 tests

---
+++ src/month_3/day25/day_25.test.ts
@@ -0,0 +1,12 @@
+import { nonZeroProduct } from "./day_25"
+
+describe('Day 25 tests', () => {
+  test.each([
+    [[1,2,3,4,4],96],
+    [[2,0,4,0,8],64],
+    [[0,1,2,3,4],24],
+    [[5,0],5],
+  ])('%j should retrun %i', (input, output) => {
+    expect(nonZeroProduct(input)).toEqual(output)
+  })
+})
