61ff070737eaa8d64b520673c73457079a376dbd
David Martin <david.martin@tookay.net>
2025-03-30 18:56:39 +0100

    test: add null return test case

--- src/month_3/day30/day_30.test.ts
+++ src/month_3/day30/day_30.test.ts
@@ -5,6 +5,7 @@ describe ('Day 30 tests', () => {
     [[0,7,4,3,10,1],7],
     [[0,1,0,1,0,1],0],
     [[1,0,10,1,2],2],
+    [[1],null],
   ])('expect %j to equal %i', (input,output) => {
     expect(secondLargest(input)).toEqual(output)
   })
