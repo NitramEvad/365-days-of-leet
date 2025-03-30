9af8811e1204d0e770bf6a21d1c1a3c589d7bd26
David Martin <david.martin@tookay.net>
2025-03-30 18:56:57 +0100

    refactor: simplify

--- src/month_3/day30/day_30.ts
+++ src/month_3/day30/day_30.ts
@@ -13,20 +13,7 @@
  * Write a JavaScript function that takes an array of numbers and returns the second largest number in the array. If the array has less than two distinct numbers, return null.
  */
 
-export function secondLargest (input:number[]):number {
-
-  let largest = input[0] > input[1] ? input[0] : input[1] 
-  let secondLargest = input[0] < input[1] ? input[0] : input[1]
-
-  input.forEach((el) => {
-    if (el > largest) {
-      secondLargest = largest
-      largest = el
-    } else if (el > secondLargest && el < largest) {
-      secondLargest = el
-    }
-  })
-  
-  return secondLargest
+export function secondLargest (input:number[]):number|null {
+  const formattedArr = [... new Set(input)].sort((a,b) => b-a)
+  return formattedArr.length > 1 ? formattedArr[1] : null
 }
-
