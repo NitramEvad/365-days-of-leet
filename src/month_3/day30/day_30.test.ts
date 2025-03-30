9c05fd4ab6763f3693705560dddbb5a63a24f9af
David Martin <david.martin@tookay.net>
2025-03-30 18:51:15 +0100

    test: day 30 tests

---
+++ src/month_3/day30/day_30.test.ts
@@ -0,0 +1,11 @@
+import { secondLargest } from "./day_30";
+
+describe ('Day 30 tests', () => {
+  test.each([
+    [[0,7,4,3,10,1],7],
+    [[0,1,0,1,0,1],0],
+    [[1,0,10,1,2],2],
+  ])('expect %j to equal %i', (input,output) => {
+    expect(secondLargest(input)).toEqual(output)
+  })
+})
