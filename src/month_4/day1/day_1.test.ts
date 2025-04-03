44a082fc8960c1fb7da4a997d9ff5143f8d9e64d
David Martin <david.martin@tookay.net>
2025-04-03 17:58:11 +0100

    test: day 1 test update

--- src/month_4/day1/day_1.test.ts
+++ src/month_4/day1/day_1.test.ts
@@ -2,9 +2,9 @@ import { smallestInt } from "./day_1";
 
 describe('Month 4, day 1 test', () => {
   test.each([
-    [[10,5,4,3,2,1],1],
-    [[10,5,4,3,2,1,-5],1],
-    [[10,5,5,5,5,5,4,3,2,6],2],
+    [[10,5,4,3,2,1,0],6],
+    [[10,5,4,3,0,2,1,-5],6],
+    [[10,5,5,5,0,5,5,4,3,2,6],1],
   ])('%j should return %i', (input, output) => {
     expect(smallestInt(input)).toEqual(output)
   })
