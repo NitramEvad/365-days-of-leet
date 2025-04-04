05dc91d8fe0bc77f80b1ea0a9e148510670b1a3a
David Martin <david.martin@tookay.net>
2025-04-04 14:47:06 +0100

    test: day 2 test extra

--- src/month_4/day2/day_2.test.ts
+++ src/month_4/day2/day_2.test.ts
@@ -2,7 +2,8 @@ import { recursion } from "./day_2";
 
 describe ('Day 2 tests', () => {
   test.each([
-    [[],5],
+    [[1,2,[3,4,5],4,5,[1,2,[3,[4,5,[6,7]]]],6],5],
+    [[1,2,[1,2,3,[1,2,3,4],4,5],3,4],3],
   ])('Expect %j to return %i', (input, output) => {
     expect(recursion(input)).toEqual(output)
   })
