7b803e69c88561aedd5cb7710bc989bf8eedf3f5
David Martin <david.martin@tookay.net>
2025-04-07 18:52:24 +0100

    feat: day 6 challenge

---
+++ src/month_4/day6/day_6.ts
@@ -0,0 +1,34 @@
+/**
+ * Daily JavaScript Challenge: Check if a Number is a Happy Number
+
+The Challenge
+Difficulty: Medium
+
+Topic: Mathematics
+
+Description
+A happy number is a number which eventually reaches 1 when replaced repeatedly by the sum of the square of its digits. If it loops endlessly in a cycle, then it is not a happy number. For example, starting with 19, the sequence is: 1² + 9² = 82, 8² + 2² = 68, 6² + 8² = 100, 1² + 0² + 0² = 1. Hence, 19 is a happy number. Implement a function to check if a given number is a happy number.
+
+ */
+
+export function happyNumber (input:number):boolean {
+
+  const seen = new Set();
+
+  while (input !== 1) {
+    if (seen.has(input)) return false
+
+    seen.add(input)
+    
+    let number1 = Math.pow(+input.toString().charAt(0),2)
+    let number2 = Math.pow(+input.toString().charAt(1),2)
+    let total = number1 + number2;
+      
+    input = total
+  }
+
+  return true
+}
+
+console.log(happyNumber(19))
+console.log(happyNumber(18))
