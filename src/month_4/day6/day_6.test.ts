d572540cef68c0abc4ac0cd1f329db8c459df6c5
David Martin <david.martin@tookay.net>
2025-04-07 18:52:10 +0100

    test: day 6

---
+++ src/month_4/day6/day_6.test.ts
@@ -0,0 +1,17 @@
+import { happyNumber } from './day_6';
+
+describe('Day 6 tests', () => {
+  test.each([
+    [19, true],
+    [7, true],
+    [10, true],
+    [13, true],
+    [49, true],
+    [17, false],
+    [18, false],
+    [20, false],
+    [11, false],
+  ])('"%i" should return "%s"', (input, output) => {
+    expect(happyNumber(input)).toEqual(output)
+  })
+})
