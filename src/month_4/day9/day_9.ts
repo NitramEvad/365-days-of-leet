3c5d29ba4326e38351e915f9d81b627cc5e0846f
David Martin <david.martin@tookay.net>
2025-04-09 14:41:48 +0100

    refactor: make "tail" recursive

--- src/month_4/day9/day_9.ts
+++ src/month_4/day9/day_9.ts
@@ -12,8 +12,10 @@ Description
 Implement a function that calculates the factorial of a given positive integer using tail recursion. A factorial of a number n is the product of all positive integers less than or equal to n. The challenge is to implement this using a tail recursive approach.
  */
 
-export function factorial (input:number):number {
-  if (input === 0) return 1
+export function factorial (input:number, total:number = 1):number {
+  if (input === 0) return total
 
-  return input * factorial(input-1)
+  return factorial(input-1, total * input)
 }
+
+factorial(5)
