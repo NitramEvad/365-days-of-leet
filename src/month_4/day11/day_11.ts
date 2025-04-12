367709153f269d05e0fad3ef7ee6db9abc9b9eb2
David Martin <david.martin@tookay.net>
2025-04-12 14:40:11 +0100

    feat: day 11 challenge

--- src/month_4/day11/day_11.ts
+++ src/month_4/day11/day_11.ts
@@ -11,3 +11,19 @@ Topic: Array Manipulation
 Description
 Given an array of dates represented as strings in 'YYYY-MM-DD' format, determine the longest chain of consecutive days present in the list. Consecutive days are defined as days that follow one another without any breaks (including weekends and holidays). The function should return the length of this longest chain.
  */
+
+export function dateFormatter (input: string[]):number {
+  let dateGap = 0;
+  const MILLISECOND_DAYS = 1000 * 60 * 60 * 24
+
+  input.reduce((acc, cur) => {
+    const dateStart = new Date(acc)
+    const dateFinish = new Date(cur)
+    const dateDifference = (dateFinish.getTime() - dateStart.getTime()) / MILLISECOND_DAYS
+
+    if (dateDifference > dateGap) dateGap = dateDifference; 
+    return cur
+  })
+
+  return dateGap
+}
