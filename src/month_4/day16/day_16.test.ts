b8b983dfcf55bc1b954d1fa0d669325ec938c3b8
David Martin <david.martin@tookay.net>
2025-04-15 16:07:25 +0100

    fix: corrects import

--- src/month_4/day16/day_16.test.ts
+++ src/month_4/day16/day_16.test.ts
@@ -1,4 +1,4 @@
-import { flattenArr } from './day_15'
+import { flattenArr } from './day_16'
 describe ('Day 15 tests', () => {
   test.each([
     [[1,2,3,[4,5,6,[111,112,113,114]],[10,20,30]],[1,2,3,4,5,6,111,112,113,114,10,20,30]],
