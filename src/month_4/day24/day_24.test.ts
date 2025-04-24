ab10989ea1f2b84a8ec1e5395c5442872d80da6b
David Martin <david.martin@tookay.net>
2025-04-24 09:42:02 +0100

    test: day 24

---
+++ src/month_4/day24/day_24.test.ts
@@ -0,0 +1,11 @@
+import { shuffle } from "./day_24";
+
+describe ('Day 24 tests', () => {
+  test.each([
+    [[0,1,2,3,4,5,6,7],[1,0,3,2,5,4,7,6]],
+    [['a','b','c','d','e','f'],['b','a','d','c','f','e']],
+    [[1,'a',2,'b',3,'c'],['a',1,'b',2,'c',3]],
+  ])('expect %j to equal %j', (input,output) => {
+    expect(shuffle(input)).toEqual(output)
+  })
+})
