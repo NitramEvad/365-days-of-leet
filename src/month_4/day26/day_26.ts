fbad3bf8190e8a5dc2d577663144fad9b433ba99
David Martin <david.martin@tookay.net>
2025-04-26 23:36:51 +0100

    feat: day 26

---
+++ src/month_4/day26/day_26.ts
@@ -0,0 +1,16 @@
+/**
+The Challenge
+Difficulty: Medium
+
+Topic: String Manipulation
+
+Description
+Write a JavaScript function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+ */
+
+
+export function longestCommonPrefix (input: string[]):string  {
+
+  return 'xyz'
+
+}
