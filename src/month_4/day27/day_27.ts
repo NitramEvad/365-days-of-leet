dfe08c426d8c7fa9e4dedc2d61df823312b15735
David Martin <david.martin@tookay.net>
2025-04-28 23:10:23 +0100

    feat: day 27 boilerplate

---
+++ src/month_4/day27/day_27.ts
@@ -0,0 +1,16 @@
+/**
+The Challenge
+Difficulty: Medium
+
+Topic: String Manipulation
+
+Description
+Write a function that takes a string as input and returns the first word that does not repeat in the sentence. Words are sequences of characters separated by space, and the function should be case-sensitive.
+ */
+
+
+export function firstNonRepeatingWord (input: string):string  {
+
+  return 'abc'
+
+}
