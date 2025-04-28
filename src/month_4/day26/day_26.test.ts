bddeb26b69da2443ccefbe791161ba2a4c9f2d48
David Martin <david.martin@tookay.net>
2025-04-28 23:10:04 +0100

    fix: small test change

--- src/month_4/day26/day_26.test.ts
+++ src/month_4/day26/day_26.test.ts
@@ -3,7 +3,7 @@ import { longestCommonPrefix } from "./day_26";
 describe('Day 26 tests', () => {
   test.each([
     [['','',''],'xyz']
-  ])('expect %j to return %j', (input,output) => {
+  ])('expect %j to return %s', (input,output) => {
     expect(longestCommonPrefix(input)).toEqual(output)
   })
 })
