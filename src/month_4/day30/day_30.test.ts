208983640aafb1e5db64bef57459efff263453ef
David Martin <david.martin@tookay.net>
2025-05-05 22:46:10 +0100

    feat: day 30 boilerplate

---
+++ src/month_4/day30/day_30.test.ts
@@ -0,0 +1,9 @@
+import { stringCompression } from "./day_30";
+
+describe('Day 30 tests', () => {
+  test.each([
+    ['aabcccccaaa','a2b1c5a3'],
+  ])('expect "%s" to return "%s"', (input,output) => {
+    expect(stringCompression(input)).toEqual(output)
+  })
+})
---
+++ src/month_4/day30/day_30.ts
@@ -0,0 +1,16 @@
+/**
+The Challenge
+Difficulty: Medium
+
+Topic: String Manipulation
+
+Description
+Implement a string compression function that uses the counts of repeated characters. For character sequence, if the character is repeated, it should be compressed into the format character+count. For example, the string "aabcccccaaa" should become "a2b1c5a3". If the compressed string is not smaller than the original, return the original string.
+ */
+
+
+export function stringCompression (input: string):string  {
+
+  return 'a2b1c5a3'
+
+}
