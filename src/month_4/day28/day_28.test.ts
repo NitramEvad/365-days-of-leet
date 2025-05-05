242979eb184435fa1a9a9b8a714777ea87208ad3
David Martin <david.martin@tookay.net>
2025-05-05 22:45:48 +0100

    feat: day 28 boilerplate

---
+++ src/month_4/day28/day_28.test.ts
@@ -0,0 +1,10 @@
+import { returnMedian } from "./day_28";
+
+describe('Day 28 tests', () => {
+  test.each([
+    [[8,9,10,12,40,50,60],12],
+    [[8,9,10,11,13,40,50,60],12],
+  ])('expect %j to return %i', (input,output) => {
+    expect(returnMedian(input)).toEqual(output)
+  })
+})
---
+++ src/month_4/day28/day_28.ts
@@ -0,0 +1,16 @@
+/**
+The Challenge
+Difficulty: Medium
+
+Topic: Array Manipulation
+
+Description
+Write a function that takes an array of numbers and returns the median value. The median is the middle number in a sorted list of numbers. If the list is even, the median is the average of the two middle numbers.
+ */
+
+
+export function returnMedian (input: number[]):number  {
+
+  return 12
+
+}
