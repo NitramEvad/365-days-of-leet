d54d43836633b4ac4b72abfe9c2d6dac0d72df81
David Martin <david.martin@tookay.net>
2025-05-07 00:01:58 +0100

    test: add test case

--- src/month_4/day26/day_26.test.ts
+++ src/month_4/day26/day_26.test.ts
@@ -2,7 +2,8 @@ import { longestCommonPrefix } from "./day_26";
 
 describe('Day 26 tests', () => {
   test.each([
-    [['cbcaxyz', 'cb123', 'cbcdnpo', 'cbcdeabbba'],['cb']]
+    [['cbcaxyz', 'cb123', 'cbcdnpo', 'cbcdeabbba'],'cb'],
+    [['cbcaxyz', 'xcb123', 'cbcdnpo', 'cbcdeabbba'],'']
   ])('expect "%s" to return "%s"', (input,output) => {
     expect(longestCommonPrefix(input)).toEqual(output)
   })
