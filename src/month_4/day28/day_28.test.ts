6a1b930b9d342e09daf14b58a85fc82e9ad303a4
David Martin <david.martin@tookay.net>
2025-05-07 22:56:57 +0100

    test: day 28

--- src/month_4/day28/day_28.test.ts
+++ src/month_4/day28/day_28.test.ts
@@ -4,6 +4,7 @@ describe('Day 28 tests', () => {
   test.each([
     [[8,9,10,12,40,50,60],12],
     [[8,9,10,11,13,40,50,60],12],
+    [[8,40,10,12,9,50,60,1],11],
   ])('expect %j to return %i', (input,output) => {
     expect(returnMedian(input)).toEqual(output)
   })
