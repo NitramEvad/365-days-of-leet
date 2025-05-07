b60a008ea05a647bd5243de84bad933c25c435dc
David Martin <david.martin@tookay.net>
2025-05-07 22:31:29 +0100

    test: day 30 test

--- src/month_4/day30/day_30.test.ts
+++ src/month_4/day30/day_30.test.ts
@@ -3,6 +3,7 @@ import { stringCompression } from "./day_30";
 describe('Day 30 tests', () => {
   test.each([
     ['aabcccccaaa','a2b1c5a3'],
+    ['abcdefg','a1b1c1d1e1f1g1']
   ])('expect "%s" to return "%s"', (input,output) => {
     expect(stringCompression(input)).toEqual(output)
   })
