87635d0e673e1473bdaefe448e9d53d36faab65f
David Martin <david.martin@tookay.net>
2025-05-07 22:57:02 +0100

    feat: day 28

--- src/month_4/day28/day_28.ts
+++ src/month_4/day28/day_28.ts
@@ -8,9 +8,15 @@ Description
 Write a function that takes an array of numbers and returns the median value. The median is the middle number in a sorted list of numbers. If the list is even, the median is the average of the two middle numbers.
  */
 
-
 export function returnMedian (input: number[]):number  {
 
-  return 12
+  const orderedList = input.sort((a,b) => a-b)
+  const length = input.length
+
+  let mid = length % 2 !== 0 
+  ? orderedList[Math.floor(length/2)] 
+  : (orderedList[(length/2)-1] + orderedList[(length/2)])/2
+
+  return mid
 
 }
