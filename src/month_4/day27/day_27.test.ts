29716bf7d5c823608742f0d62a5d72468c3ab431
David Martin <david.martin@tookay.net>
2025-05-07 22:43:59 +0100

    test: day 27

--- src/month_4/day27/day_27.test.ts
+++ src/month_4/day27/day_27.test.ts
@@ -2,7 +2,9 @@ import { firstNonRepeatingWord } from "./day_27";
 
 describe('Day 27 tests', () => {
   test.each([
-    ['aabcccccaaa','a2b1c5a3']
+    ['the quick brown fox jumps','the'],
+    ['quick quick the brown fox jumps','the'],
+    ['quick quick brown brown the fox jumps','the'],
   ])('expect "%s" to return "%s"', (input,output) => {
     expect(firstNonRepeatingWord(input)).toEqual(output)
   })
