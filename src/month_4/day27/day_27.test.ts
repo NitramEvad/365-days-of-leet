a4060ce8aeeb6d0f1b1945a42e9df8d4204f73bb
David Martin <david.martin@tookay.net>
2025-05-07 22:31:17 +0100

    test: day 27

--- src/month_4/day27/day_27.test.ts
+++ src/month_4/day27/day_27.test.ts
@@ -2,7 +2,7 @@ import { firstNonRepeatingWord } from "./day_27";
 
 describe('Day 27 tests', () => {
   test.each([
-    ['xyz xyz abc xyz nbc','abc']
+    ['aabcccccaaa','a2b1c5a3']
   ])('expect "%s" to return "%s"', (input,output) => {
     expect(firstNonRepeatingWord(input)).toEqual(output)
   })
