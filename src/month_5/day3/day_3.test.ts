3280cbb0bf86e61807782c8997fa37055d3ef474
David Martin <david.martin@tookay.net>
2025-05-10 18:07:03 +0100

    test: day 3 map array to object

--- src/month_5/day3/day_3.test.ts
+++ src/month_5/day3/day_3.test.ts
@@ -1,9 +1,10 @@
-import { x } from "./day_3";
+import { arrayMapping } from "./day_3";
 
 describe('Day 3 tests', () => {
   test.each([
-    ['aabcccccaaa','a2b1c5a3'],
-  ])('expect "%s" to return "%s"', (input,output) => {
-    expect(x(input)).toEqual(output)
+    [[1,2,3,2,1,2,4, 'a','b','a'],{1: 2, 2: 3, 3: 1, 4: 1, 'a': 2, 'b': 1}],
+    [['a','a','b'],{'a':2, 'b':1}]
+  ])('expect %j to return "%j"', (input,output) => {
+    expect(arrayMapping(input)).toEqual(output)
   })
 })
