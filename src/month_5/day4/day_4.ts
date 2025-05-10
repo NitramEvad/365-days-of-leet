15ee6ab3abaf2389162ee9e11c15433d64d6124e
David Martin <david.martin@tookay.net>
2025-05-10 19:12:14 +0100

    feat: (WIP) day 4

--- src/month_5/day4/day_4.ts
+++ src/month_5/day4/day_4.ts
@@ -2,15 +2,31 @@
 The Challenge
 Difficulty: Medium
 
-Topic: Array Manipulation
+Topic: Number Theory
 
 Description
-Given an array of integers, your task is to find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times in the array, where n is the length of the array. You can assume that the array always has a majority element.
+Implement a function to determine if a number is a happy number. A happy number is defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers.
  */
 
 
-export function x (input: string):string  {
+export function happyNumber (input: number):boolean  {
 
-  return 'a2b1c5a3'
+  let processed = 0;
+
+  while (processed !== 1) {
+    processed = input
+      .toString()
+      .split('')
+      .reduce((acc, cur) => {
+        console.log(acc,cur)
+        return acc + (Number(cur) * Number(cur))
+      },0)
+
+  console.log(processed)
+  }
+  
+  return true
 
 }
+
+console.log(happyNumber(23)) // 2,3=4+9=13; 1,3=1+9=10; 1,0=1+0=1
