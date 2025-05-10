2574c50a980a1aed50debc577ab22214bf3ea64a
David Martin <david.martin@tookay.net>
2025-05-10 17:54:26 +0100

    test: day 2 tests

--- src/month_5/day2/day_2.test.ts
+++ src/month_5/day2/day_2.test.ts
@@ -1,9 +1,18 @@
-import { x } from "./day_2";
+import { primeAgen } from "./day_2";
 
 describe('Day 2 tests', () => {
   test.each([
-    ['aabcccccaaa','a2b1c5a3'],
-  ])('expect "%s" to return "%s"', (input,output) => {
-    expect(x(input)).toEqual(output)
+    [1, 0],
+    [2, 2],
+    [3, 5],
+    [4, 5],
+    [5, 10],
+    [6, 10],
+    [7, 17],
+    [8, 17],
+    [9, 17],
+    [10, 17],
+  ])('expect %i to return %i', (input,output) => {
+    expect(primeAgen(input)).toEqual(output)
   })
 })
