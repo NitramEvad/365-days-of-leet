1c39e4a5360ce1ab8716000d8a9dca737a286546
David Martin <david.martin@tookay.net>
2025-05-10 19:12:02 +0100

    tests: day 4 boilerplate

--- src/month_5/day4/day_4.test.ts
+++ src/month_5/day4/day_4.test.ts
@@ -1,9 +1,9 @@
-import { x } from "./day_4";
+import { happyNumber } from "./day_4";
 
 describe('Day 4 tests', () => {
   test.each([
-    ['aabcccccaaa','a2b1c5a3'],
-  ])('expect "%s" to return "%s"', (input,output) => {
-    expect(x(input)).toEqual(output)
+    [23,true],
+  ])('expect %i to return %s', (input,output) => {
+    expect(happyNumber(input)).toEqual(output)
   })
 })
