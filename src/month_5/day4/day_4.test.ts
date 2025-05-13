6a570ce972acefb281d11e3a288615d1f5214071
David Martin <david.martin@tookay.net>
2025-05-13 16:41:20 +0100

    test: improve happy number tests

--- src/month_5/day4/day_4.test.ts
+++ src/month_5/day4/day_4.test.ts
@@ -2,7 +2,17 @@ import { happyNumber } from "./day_4";
 
 describe('Day 4 tests', () => {
   test.each([
+    [1,true],
+    [7,true],
+    [10,true],
+    [13,true],
     [23,true],
+    [86,true],
+    [100,true],
+    [2,false],
+    [3,false],
+    [24,false],
+    [26,false],
   ])('expect %i to return %s', (input,output) => {
     expect(happyNumber(input)).toEqual(output)
   })
