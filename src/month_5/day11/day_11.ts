ac28d16a0d654313939b73507103c53843cf2bf2
David Martin <david.martin@tookay.net>
2025-05-25 19:30:02 +0100

    feat: day 11 boilerplate

---
+++ src/month_5/day11/day_11.ts
@@ -0,0 +1,14 @@
+/**
+https://dev.to/dpc/daily-javascript-challenge-js-140-binary-gap-length-2boc
+The Challenge
+Difficulty: Medium
+
+Topic: Binary Representation
+
+Description
+Write a JavaScript function to determine the length of the longest sequence of consecutive zeros in the binary representation of a given positive integer, where the sequence is surrounded by ones at both ends.
+ */
+
+export function binaryGap (input: number):number {
+  return 5
+}
