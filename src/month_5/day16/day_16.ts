26145c6c3ce84b001ff31f26190cbff5410751d0
David Martin <david.martin@tookay.net>
2025-06-16 22:35:09 +0100

    feat: day 16 challenge

---
+++ src/month_5/day16/day_16.ts
@@ -0,0 +1,9 @@
+/**
+ * The Challenge
+Difficulty: Medium
+
+Topic: Matrix Operations
+
+Description
+Write a function that takes a 2D array (matrix) as input and returns its transpose. The transpose of a matrix is obtained by swapping the rows and columns of the matrix.
+ */
